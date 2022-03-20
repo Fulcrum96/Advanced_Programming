e8bfe075b761c882a5994cf5f88cdbc42f4054cc
Gustavo Martinez <gustavo96mm@gmail.com>
2022-03-20 11:02:51 -0600

    New menu options to home directory, changes in username input.

--- newUser.sh
+++ newUser.sh
@@ -1,18 +1,39 @@
 #!/bin/bash 
 echo "Agregar nuevo usuario al sistema."
 
-read -p "Ingrese nombre de usuario: " username
-if id -u $username >/dev/null 2>&1; then
-    echo "El usuario ya existe."
-    username=""
-fi
-echo $username
+while [ x$username = "x" ]; do
+    read -p "Ingrese nombre de usuario: " username
+    if id -u $username >/dev/null 2>&1; then
+        echo "El usuario ya existe."
+        username=""
+    fi
+done
+    echo $username
 
 read -p "Ingrese nombre completo: " realname
 
 read -sp "Ingrese la contraseña: " PASSWORD
 
-read -p "Ingrese la ubicación del directorio HOME (ej: /home/$username: " homepath
+#read -p "Ingrese la ubicación del directorio HOME (ej: /home/$username: " homepath
+#echo $homepath
+
+PS3="Seleccione la ubicación del directorio HOME: "
+homeoptions=("Default (/home$username)" "Personalizado")
+select chosenhome in "${homeoptions[@]}"; do
+    case $chosenhome in 
+        "Default (/home/$username)")
+            homepath="/home/$username"
+            break
+            ;;
+        "Personalizado")
+            read -p "Ingrese la ubicación del directorio HOME: " homepath
+            break
+            ;;
+        *)
+            echo "Opción $REPLY no válida."
+            ;;
+    esac
+done
 echo $homepath
 
 PS3="Seleccione el tipo de shell: "
@@ -28,18 +49,22 @@ select chosenshell in "${shelloptions[@]}"; do
             break
             ;;
         *)
-            echo "Opción $REPLY no válida"
+            echo "Opción $REPLY no válida."
             ;;
     esac
 done 
 echo $shelltype
 
-read -p "Ingrese el grupo al que pertenecerá el usuario: " group
-if id -g $group >dev/null 2>&1; then
-    echo "El grupo ya existe."
-else
-    groupadd $group
-fi
+while [ x$group = "x" ]; do
+    read -p "Ingrese el grupo al que pertenecerá el usuario: " group
+    if id -g $group >dev/null 2>&1; then
+        echo "El grupo ya existe."
+    else
+        groupadd $group
+    fi
+done
 echo $group
+
+
 
 useradd -g $group -s $shelltype -d $homepath -m $username
